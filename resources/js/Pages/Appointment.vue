06dfb0ffeddfafd68a6ecb11c035363a67d366a0
viennarose <viennapepito01@gmail.com>
2022-08-04 12:15:00 +0800

    first commit

---
+++ resources/js/Pages/Appointment.vue
@@ -0,0 +1,45 @@
+<template>
+ <h1>My Appointments</h1>
+ <hr>
+
+<div class="d-flex align-contents-between">
+    <div class="card mb-3" v-for="appointment in appointments" :key="appointment.id">
+        <div class="card-header">
+            <h5>Appointment Number: {{appointment.id}}</h5>
+        </div>
+        <div class="card-body" >
+            <h6>Patient Information</h6>
+            <table class="table">
+                <tr>
+                    <th>Name:</th>
+                    <td>{{appointment.name}}</td>
+                </tr>
+                <tr>
+                    <th>Contact:</th>
+                    <td>{{appointment.contact}}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup>
+import Layout from '../Shared/Layout.vue'
+
+</script>
+<script>
+export default {
+    layout: Layout,
+    props: {
+        appointments: Array
+    }
+}
+</script>
+
+<style scoped>
+.card{
+    width: 375px;
+
+}
+</style>
